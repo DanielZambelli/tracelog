095abef8477d6525663897b5700c1b73503a066b
Daniel Zambelli <mail@danielzambelli.dk>
2020-06-23 13:33:31 +0200

    depricated removed

--- client/src/state/actionCreators.js
+++ client/src/state/actionCreators.js
@@ -6,8 +6,8 @@ export * from './globalState/actionCreators'
 export * from './tracelogs/actionCreators'
 export const clearState = () => ({ type: CLEAR_STATE })
 
-const API = process.env.REACT_APP_API
 
+const API = process.env.REACT_APP_API
 const { Base64 } = require('js-base64')
 
 export const authenticate = (secret) => (dispatch) => {
@@ -29,17 +29,3 @@ export const fetchLogs = () => (dispatch, getState) => {
     .then(res => res.json())
     .then(tracelogs => dispatch(setTracelogs(tracelogs)))
 }
-
-
- // when new tracelogs are added the backend will pushed
-  // these via socket io to the frontend client
-  // TODO: action creator
-  // subscribeToPostLog(secret){
-  //   const { unshiftTracelog } = this.props
-  //   const { socket } = window
-  //   if(socket){
-  //     // TODO: just pass _siid value
-  //     socket.emit('JOIN_TRACELOG_ROOM', { secret })
-  //     socket.on('POST_LOG', unshiftTracelog)
-  //   }
-  // }
